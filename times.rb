aef28f21348f3ddde89d2ecd0774741d7baaa3fb
Laine Barrett <laine.barrett2019@gmail.com>
2020-05-14 20:41:57 +0000

    Automatically backed up by Learn

--- times.rb
+++ times.rb
@@ -1,3 +1,9 @@
 def using_times
   puts "Wingardium Leviosa"
+  puts "Wingardium Leviosa"
+  puts "Wingardium Leviosa"
+  puts "Wingardium Leviosa"
+  puts "Wingardium Leviosa"
+  puts "Wingardium Leviosa"
+  puts "Wingardium Leviosa"
 end
